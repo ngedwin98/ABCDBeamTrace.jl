8f16f1e17d64d6655ceafd7e698439d79df61b5d
ngedwin98 <ngedwin98@gmail.com>
2018-06-24 02:32:27 -0700

    Add support for curved interface
    Uses matrices found in doi:10.1364/AO.26.000427

--- src/elements.jl
+++ src/elements.jl
@@ -3,14 +3,15 @@ struct FreeSpace <: Element
     L::Real
 end
 struct Interface <: Element
-    η::Real; θ::Real
+    η::Real; θ::Real; R::Real # R > 0 when light hits concave side
 end
 struct ThinLens <: Element
     f::Real; θ::Real
 end
-Interface(η::Real) = Interface(η,0)
 ThinLens(f::Real) = ThinLens(f,0)
-Mirror(R::Real,θ::Real) = ThinLens(R/2,θ)
+Interface(η::Real) = Interface(η,0,Inf)
+
+Mirror(R::Real, θ::Real) = ThinLens(R/2,θ)
 FreeSpace(L::Real, N::Int) = fill(FreeSpace(L/N), N)
 FreeSpace(L::Real, dz::Real) = FreeSpace(L, (Int∘round)(L/dz))
 
@@ -22,17 +23,21 @@ struct Sag{E} <: Element where {E<:Element}
 end
 Tan(e::FreeSpace) = e
 Sag(e::FreeSpace) = e
-Sag(e::Interface) = e
 
 RTM(e::FreeSpace) = [1 e.L ; 0 1]
-RTM(e::Interface) = [1 0 ; 0 e.η]
+RTM(e::Interface) = [1 0 ; (e.η-1)/e.R e.η]
 RTM(e::ThinLens) = [1 0 ; -1/e.f 1]
 RTM(e::Tan{ThinLens}) = (RTM∘ThinLens)(e.e.f*cos(e.e.θ))
 RTM(e::Sag{ThinLens}) = (RTM∘ThinLens)(e.e.f/cos(e.e.θ))
 function RTM(e::Tan{Interface})
-    θ1, η = e.e.θ, e.e.η; θ2 = asin(η*sin(θ1))
-    return [cos(θ2)/cos(θ1) 0 ; 0 η*cos(θ1)/cos(θ2)]
+    θ1, η, R = e.e.θ, e.e.η, e.e.R; θ2 = asin(η*sin(θ1))
+    return [cos(θ2)/cos(θ1) 0 ; (cos(θ2)-η*cos(θ1))/(R*cos(θ1)*cos(θ2)) η*cos(θ1)/cos(θ2)]
 end
+function RTM(e::Sag{Interface})
+    θ1, η, R = e.e.θ, e.e.η, e.e.R; θ2 = asin(η*sin(θ1))
+    return [1 0 ; (cos(θ2)-η*cos(θ1))/R η]
+end
+# See doi:10.1364/AO.26.000427 for these matrices
 
 dΓ(e::Element) = (0,1)
 dΓ(e::Union{Tan,Sag}) = dΓ(e.e)
