92f8e8554f3d1cbca8a2ce76bd1bc242b8ca5e3e
Edwin Ng <ngedwin98@gmail.com>
2018-09-22 16:16:35 -0700

    Refactor some code and update/modify nonexported functions

--- src/beamtrace.jl
+++ src/beamtrace.jl
@@ -4,24 +4,30 @@ struct Beam
 end
 Beam(λ::Real, w0::Real, n0::Real=1) = Beam(λ, 0, n0, 0, 0, 1im*π*n0*w0^2/λ)
 
-function Mobius(e::Element)
-    M = RTM(e); dz,η = dΓ(e)
-    return function(Γ::Beam)
-        return Beam(Γ.λ, Γ.z+dz, Γ.n/η, M*[Γ.x,Γ.k]..., /(M*[Γ.q,1]...))
-    end
+η(e::Interface) = e.η
+η(e::Union{Tan,Sag}) = dη(e.e)
+η(e::Element) = 1
+
+dz(e::FreeSpace) = e.L
+dz(e::Union{Tan,Sag}) = dz(e.e)
+dz(e::Element) = 0
+
+function transform(e::Element, Γ::Beam)
+    M = RTM(e)
+    return Beam(Γ.λ, Γ.z+dz(e), Γ.n/η(e), M*[Γ.x,Γ.k]..., /(M*[Γ.q,1]...))
 end
 
 function beamtrace(elems::Vector{<:Element}, Γ0::Beam)
-    Γ = Vector{Beam}(length(elems)+1); Γ[1] = Γ0
-    for (ind, e) in enumerate(elems)
-        Γ[ind+1] = Mobius(e)(Γ[ind])
+    Γs = Vector{Beam}(length(elems)+1); Γs[1] = Γ0
+    for (ind, elem) in enumerate(elems)
+        Γs[ind+1] = transform(elem, Γs[ind])
     end
-    return Γ
+    return Γs
 end
-
-discretize(e::Element, N::Int) = e
-discretize(e::FreeSpace, N::Int) = FreeSpace(e.L, N)
-discretize(els::Vector{<:Element}, N::Int) = vcat([discretize.(e,N) for e in els]...)
 
 spotsize(Γ::Beam) = /(-Γ.λ, π*Γ.n*imag(1/Γ.q)) |> sqrt
 location(Γ::Beam) = Γ.z
+
+discretize(e::FreeSpace, N::Int) = fill(FreeSpace(L/N), N)
+discretize(e::Element, N::Int) = e
+discretize(els::Vector{<:Element}, N::Int) = vcat(discretize.(e,N)...)
--- src/elements.jl
+++ src/elements.jl
@@ -12,8 +12,6 @@ ThinLens(f::Real) = ThinLens(f,0)
 Interface(η::Real) = Interface(η,0,Inf)
 
 Mirror(R::Real, θ::Real) = ThinLens(R/2,θ)
-FreeSpace(L::Real, N::Int) = fill(FreeSpace(L/N), N)
-FreeSpace(L::Real, dz::Real) = FreeSpace(L, (Int∘round)(L/dz))
 
 struct Tan{E} <: Element where {E<:Element}
     e::E
@@ -29,17 +27,13 @@ RTM(e::Interface) = [1 0 ; (e.η-1)/e.R e.η]
 RTM(e::ThinLens) = [1 0 ; -1/e.f 1]
 RTM(e::Tan{ThinLens}) = (RTM∘ThinLens)(e.e.f*cos(e.e.θ))
 RTM(e::Sag{ThinLens}) = (RTM∘ThinLens)(e.e.f/cos(e.e.θ))
+# See doi:10.1364/AO.26.000427 for the following matrices
 function RTM(e::Tan{Interface})
     θ1, η, R = e.e.θ, e.e.η, e.e.R; θ2 = asin(η*sin(θ1))
-    return [cos(θ2)/cos(θ1) 0 ; (cos(θ2)-η*cos(θ1))/(R*cos(θ1)*cos(θ2)) η*cos(θ1)/cos(θ2)]
+    return [cos(θ2)/cos(θ1) 0 ;
+        (cos(θ2)-η*cos(θ1))/(R*cos(θ1)*cos(θ2)) η*cos(θ1)/cos(θ2)]
 end
 function RTM(e::Sag{Interface})
     θ1, η, R = e.e.θ, e.e.η, e.e.R; θ2 = asin(η*sin(θ1))
     return [1 0 ; (cos(θ2)-η*cos(θ1))/R η]
 end
-# See doi:10.1364/AO.26.000427 for these matrices
-
-dΓ(e::Element) = (0,1)
-dΓ(e::Union{Tan,Sag}) = dΓ(e.e)
-dΓ(e::FreeSpace) = (e.L,1)
-dΓ(e::Interface) = (0,e.η)
