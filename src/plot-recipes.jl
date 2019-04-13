0dc947f6c3bde8e07b4ba8412ce02fe9ef58d7b4
Bjoern Stein <bjoern.stein@quantum-factory.de>
2019-04-13 11:04:11 +0200

    basic plot recipe for ::Vector{<:Element}

--- src/plot-recipes.jl
+++ src/plot-recipes.jl
@@ -1,2 +1,6 @@
 # plot recipes, see https://docs.juliaplots.org/latest/recipes/ and
 # https://github.com/JuliaPlots/RecipesBase.jl
+
+@recipe function f(system::Vector{<:Element})
+    return waistradiusfunc(system, beam)
+end
